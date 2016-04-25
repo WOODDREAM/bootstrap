06fbca1e7d5f8d8df2a1c84648851ec8ba5a8da2
huangtao <huangtao@2dfire.com>
2016-04-25 20:49:01 +0800

    基础页面

---
+++ resource/js/hello.js
@@ -0,0 +1,7 @@
+var http = require("juline-pv.com");
+http.createServer(function(request, response) {  
+    response.writeHead(200, {"Content-Type": "text/plain"});  
+    response.write("Hello World");  
+    response.end();
+}).listen(8888);
+console.log("nodejs start listen 8888 port!");
