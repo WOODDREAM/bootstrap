8ccc44a6aabfbc8e4ba98e31d70efbd6b51868c9
huangtao <huangtao@2dfire.com>
2016-05-11 20:23:02 +0800

    邮件

--- grade/js/grade.js
+++ grade/js/grade.js
@@ -1,19 +1,21 @@
-var type;
-$(".loginTeacher").on('click', function(){
-    type = $(".loginTeacher").attr("value");
-    alert(type+"---")
-});
 $('#login').on('click', function () {
     var mobile = $("#loginMobile").val();
-    var passWord=$("#loginPassWord").val();
-
+    var passWord = $("#loginPassWord").val();
+    var type = $("#loginTeacher").val();
     var srt = {
         mobile: mobile,
         pass_word: passWord,
         type: type
     };
-    $.get("http://juline-pv.com/user/sign_in",srt,function(data){
+    $.post("http://localhost:8080/grade-manager/user/sign_in", srt, function (data) {
+        if (!!data) {
+            if (!!data.code && data.code == 1) {
+
+            }
+        }
+        alert(data.code)
+        alert(data.data.name)
         console.log(data);
-    });
+    })
 });
 
